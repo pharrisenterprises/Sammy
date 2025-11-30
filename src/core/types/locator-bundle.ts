733c73700a6f49ff609705901eafe7cb39889894
pharrisenterprises <pharrisenterprises@gmail.com>
2025-11-30 12:36:05 +0000

    chore: Add LocatorBundle stub type for Step dependencies
    - Create minimal LocatorBundle interface stub
    - Will be replaced with full implementation in subsequent prompts

---
+++ src/core/types/locator-bundle.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview LocatorBundle type stub for Step dependencies
+ * @module core/types/locator-bundle
+ */
+
+export interface LocatorBundle {
+  tag?: string;
+  [key: string]: any;
+}
